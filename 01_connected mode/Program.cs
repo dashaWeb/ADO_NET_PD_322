9f00d38d8a53dd09231f6d032879b9592cb34953
konopelko <konopelko@A26PC1>
2024-03-04 17:33:34 +0200

    Add project files.

---
+++ 01_connected mode/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_connected_mode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connection String - містить всю інформацію для підключення до сервера 
+            // SQL Server
+            // -- Windows Authentication -- 
+            // "Data Source=server_name;Initial Catalog=db_name;Integrated Security=True"
+            // -- Server Authentication -- 
+            // "Data Source=server_name;Initial Catalog=db_name;Integrated Security=False;User ID=login;Password=password"
+
+            string conn = @"Data Source=(localdb)\ProjectModels;Initial Catalog=SportShop;Integrated Security=True;Connect Timeout=2;";
+            // default Connect Timeout = 30
+            SqlConnection connection = new SqlConnection(conn);
+            connection.Open();
+            Console.WriteLine("Connected");
+
+
+            /*string cmdText = @"insert into Products
+                                values('Зошит', 'Школа', 50, 10, 'Україна', 5)";
+
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            // ExecuteNonQuery - викoнує команду, яка не повертає результату (insert delete, update), але метод повертає кількість рядків, які були задіяні в команді
+            int row = command.ExecuteNonQuery();
+            Console.WriteLine($"{row} rows affected");*/
+
+            // ExecuteScalar - виконує команду, яка повертає одне значення
+            /*string cmdText = @"select AVG(Price) from Products";
+            SqlCommand command = new SqlCommand(cmdText,connection);
+            var res = (int)command.ExecuteScalar();
+            Console.WriteLine($"Result avg price :: {res}");*/
+
+            //ExecuteReader - виконує команду select та повертає результат у вигляді DbDataReader
+            string cmdText = "select* from Products";
+            SqlCommand command = new SqlCommand(cmdText,connection);
+            var reader = command.ExecuteReader();
+
+            Console.OutputEncoding = Encoding.UTF8;
+            // відображається назва всіх колонок
+            for (int i = 0;i < reader.FieldCount; i++) // FieldCount - кількість стовпців у таблиці
+            {
+                Console.Write($"{reader.GetName(i),16}");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-',120));
+            // відображаємо всі значення кожного рядка
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i],16}");
+                }
+                Console.WriteLine();
+            }
+            connection.Close();
+
+            // name; drop database Products
+        }
+    }
+}
---
+++ 02_params/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_params
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int CostPrice { get; set; }
+        public string Producer { get; set; }
+        public int Price { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["connShop"].ConnectionString;
+            SqlConnection connection = new SqlConnection(conn);
+            connection.Open();
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            //string name = Console.ReadLine();
+            //ShowProductByNameUnSafe(name, connection);
+            //ShowProductByName(name, connection);
+            AddProduct(new Product()
+            {
+                Name = "Шарф",
+                Type = "Аксесуари",
+                Quantity = 15,
+                CostPrice= 50,
+                Price = 150,
+                Producer = "Україна"
+            }, connection);
+            connection.Close();
+        }
+        // варіант передачі параметрів в запиті, якій містить велику 'дирку' в безпеці
+        static void ShowProductByNameUnSafe(string name, SqlConnection connection)
+        {
+            // name = "test; drop table Products"
+            string cmdText = $@"select * from Products where Name = '{name}'";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            var reader = command.ExecuteReader();
+            ShowReaderData(reader);
+        }
+        static void ShowProductByName(string name, SqlConnection connection)
+        {
+            string cmdText = $@"select * from Products where Name = @prod_name";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            command.Parameters.Add("@prod_name", System.Data.SqlDbType.NVarChar).Value = name;
+            var reader = command.ExecuteReader();
+            ShowReaderData(reader);
+        }
+        static void AddProduct(Product product, SqlConnection connection)
+        {
+            string cmdText = @"insert into Products values(@name,@type,@quantity,@cost_price,@producer,@price)";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            command.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = product.Name;
+            command.Parameters.Add("@type", System.Data.SqlDbType.NVarChar).Value = product.Type;
+            command.Parameters.Add("@quantity", System.Data.SqlDbType.Int).Value = product.Quantity;
+            command.Parameters.Add("@cost_price", System.Data.SqlDbType.Int).Value = product.CostPrice;
+            command.Parameters.Add("@producer", System.Data.SqlDbType.NVarChar).Value = product.Producer;
+            command.Parameters.Add("@price", System.Data.SqlDbType.Int).Value = product.Price;
+            command.ExecuteNonQuery();
+
+        }
+        static void ShowReaderData(SqlDataReader reader)
+        {
+            for (int i = 0; i < reader.FieldCount; i++) // FieldCount - кількість стовпців у таблиці
+            {
+                Console.Write($"{reader.GetName(i),16}");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 120));
+            // відображаємо всі значення кожного рядка
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i],16}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
