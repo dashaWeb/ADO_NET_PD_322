5b3e7af6414c768f3367f306879e6680de66df7f
Dasha <konopelko@step.rivne>
2024-03-08 20:37:13 +0200

    disconnected mode

---
+++ 04_connected_wpf/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using _05_Data_access.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace _04_connected_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SportShopDb db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new SportShopDb(ConfigurationManager.ConnectionStrings["connShop"].ConnectionString);
+        }
+
+        private void getProducts(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.GetAll();
+        }
+
+        private void getProduct(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(idProduct.Text);
+            List<Product> list = new List<Product>();
+
+            Product item = db.getOneProduct(id);
+            if (item != null)
+            {
+                list.Add(item);
+                dataGrid.ItemsSource = list;
+            }
+            else
+            {
+                MessageBox.Show(" id not found");
+            }
+            idProduct.Text = "";
+        }
+
+        private void Button_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.getProduct(sender, e);
+            }
+        }
+    }
+}
+
---
+++ 06_dataAdapter/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06_dataAdapter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        private SqlConnection connection;
+        private SqlDataAdapter da;
+        private DataSet set;
+        private SqlCommandBuilder cmd;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connShop"].ConnectionString);
+        }
+
+        // Fill btn
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string sql = commandText.Text;
+            da = new SqlDataAdapter(sql, connection);
+            cmd = new SqlCommandBuilder(da); // create command builder for auto generate insert, update, delete
+            set = new DataSet();
+            da.Fill(set,"MyTable");
+            dataGrid.ItemsSource = set.Tables["MyTable"].DefaultView;
+        }
+        // Update btn
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            da.Update(set,"MyTable");
+        }
+    }
+}
